9bd18851f2f27b590287d6a360e5c4af2abb000e
Gigolaev Igor <41339681+Gigaw@users.noreply.github.com>
2024-07-29 14:41:04 +0300

    useUrlAvailability is done

--- components/UrlAvailability.tsx
+++ components/UrlAvailability.tsx
@@ -1,38 +1,12 @@
 import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
-import React, { useEffect, useState } from "react";
-import NetInfo from "@react-native-community/netinfo";
+import React from "react";
+import useUrlAvailability from "@/hooks/useUrlAvailability";
 
 interface Props {
   url: string;
 }
 const UrlAvailability = ({ url }: Props) => {
-  const [isUrlAvailable, setIsUrlAvailable] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isConnected, setIsConnected] = useState<boolean | null>(null);
-
-  const checkURL = async (url: string) => {
-    setIsLoading(true);
-    try {
-      const request = await fetch(url);
-      if (request.ok) {
-        setIsUrlAvailable(true);
-      } else {
-        setIsUrlAvailable(false);
-      }
-    } catch (e) {
-      setIsUrlAvailable(false);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    const unsubscribe = NetInfo.addEventListener((state) => {
-      setIsConnected(state.isConnected);
-    });
-    checkURL(url);
-    return unsubscribe;
-  }, []);
+  const { isConnected, isLoading, isUrlAvailable } = useUrlAvailability(url);
 
   return (
     <View>
---
+++ hooks/useUrlAvailability.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import NetInfo from "@react-native-community/netinfo";
+
+const useUrlAvailability = (url: string) => {
+  const [isUrlAvailable, setIsUrlAvailable] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isConnected, setIsConnected] = useState<boolean | null>(null);
+
+  const checkURL = async (url: string) => {
+    setIsLoading(true);
+    try {
+      const request = await fetch(url);
+      if (request.ok) {
+        setIsUrlAvailable(true);
+      } else {
+        setIsUrlAvailable(false);
+      }
+    } catch (e) {
+      setIsUrlAvailable(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsConnected(state.isConnected);
+    });
+    checkURL(url);
+    return unsubscribe;
+  }, []);
+
+  return {
+    isUrlAvailable,
+    isLoading,
+    isConnected,
+  };
+};
+
+export default useUrlAvailability;
